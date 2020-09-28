2bfb58577ed54ecb569086044459f4848e7ee734
TheLastGimbus <mateusz.soszynski@tuta.io>
2020-09-29 01:33:07 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE/"README.md").read_text()
 
 setuptools.setup(
     name="dlink-scraper",
-    version="1.0.0",
+    version="1.0.1",
     description="Scraper for getting data from D-Link DWR-921 LTE router",
     long_description=README,
     long_description_content_type='text/markdown',
